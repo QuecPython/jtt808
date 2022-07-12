cd164f6a00467b1fd48d3b47fdd7894fc9cd8e8b
JackSun <jack.sun@quectel.com>
2022-07-12 10:53:38 +0800

    update: iter number range change.

--- code/jtt808.py
+++ code/jtt808.py
@@ -25,19 +25,16 @@
 """
 
 import uos
-import ure
 import usys
 import utime
-import usocket
 import osTimer
 import _thread
 from usr.logging import getLogger
-from usr.common import option_lock, Singleton
+from usr.common import SocketBase
 from usr.jt_message import DOWNLINK_MESSAGE, UPLINK_MESSAGE, JTMessageParse, set_jtmsg_config
 
 logger = getLogger(__name__)
 
-_socket_lock = _thread.allocate_lock()
 
 # These message ids's response is terminal general answer.
 GENERAL_ANSWER_MSG_ID = (
@@ -45,282 +42,6 @@ GENERAL_ANSWER_MSG_ID = (
     0x8304, 0x8400, 0x8401, 0x8600, 0x8601, 0x8602, 0x8603, 0x8604, 0x8605,
     0x8606, 0x8607, 0x8701, 0x8803, 0x8804, 0x8805, 0x8900, 0x8A00
 )
-
-
-class SocketBase(Singleton):
-    """This class is socket base"""
-
-    def __init__(self, ip=None, port=None, domain=None, method="TCP"):
-        """
-        Args:
-            ip: server ip address (default: {None})
-            port: server port (default: {None})
-            domain: server domain (default: {None})
-            method: TCP or UDP (default: {"TCP"})
-        """
-        self.__ip = ip
-        self.__port = port
-        self.__domain = domain
-        self.__addr = None
-        self.__method = method
-        self.__socket = None
-        self.__socket_args = []
-        self.__timeout = 30
-        self.__init_addr()
-        self.__init_socket()
-
-    def __init_addr(self):
-        """Get ip and port from domain.
-
-        Raises:
-            ValueError: Domain DNS parsing falied.
-        """
-        if self.__domain is not None and self.__domain:
-            if self.__port is None:
-                self.__port == 8883 if self.__domain.startswith("https://") else 1883
-            try:
-                addr_info = usocket.getaddrinfo(self.__domain, self.__port)
-                self.__ip = addr_info[0][-1][0]
-            except Exception as e:
-                usys.print_exception(e)
-                raise ValueError("Domain %s DNS parsing error. %s" % (self.__domain, str(e)))
-        self.__addr = (self.__ip, self.__port)
-
-    def __init_socket(self):
-        """Init socket by ip, port and method
-
-        Raises:
-            ValueError: ip or domain or method is illegal.
-        """
-        if self.__check_ipv4():
-            socket_af = usocket.AF_INET
-        elif self.__check_ipv6():
-            socket_af = usocket.AF_INET6
-        else:
-            raise ValueError("Args ip %s is illegal!" % self.__ip)
-
-        if self.__method == 'TCP':
-            socket_type = usocket.SOCK_STREAM
-            socket_proto = usocket.IPPROTO_TCP
-        elif self.__method == 'UDP':
-            socket_type = usocket.SOCK_DGRAM
-            socket_proto = usocket.IPPROTO_UDP
-        else:
-            raise ValueError("Args method is TCP or UDP, not %s" % self.__method)
-        self.__socket_args = (socket_af, socket_type, socket_proto)
-
-    def __check_ipv4(self):
-        """Check ip is ipv4.
-
-        Returns:
-            bool: True - ip is ipv4, False - ip is not ipv4
-        """
-        self.__ipv4_item = r"(25[0-5]|2[0-4]\d|[01]?\d\d?)"
-        self.__ipv4_regex = r"^{item}\.{item}\.{item}\.{item}$".format(item=self.__ipv4_item)
-        if self.__ip.find(":") == -1:
-            ipv4_re = ure.search(self.__ipv4_regex, self.__ip)
-            if ipv4_re:
-                if ipv4_re.group(0) == self.__ip:
-                    return True
-        return False
-
-    def __check_ipv6(self):
-        """Check ip is ipv6.
-
-        Returns:
-            bool: True - ip is ipv6, False - ip is not ipv6
-        """
-        # self.__ipv6_item = r"[0-9a-fA-F]{1:4}"
-        self.__ipv6_code = r"[0-9a-fA-F]"
-        ipv6_item_format = [self.__ipv6_code * i for i in range(1, 5)]
-        self.__ipv6_item = r"{}|{}|{}|{}".format(*ipv6_item_format)
-
-        # TODO: check ip is ipv6 by regex when ure support `{n,m}`
-        # ipv4_regex = r"({item}\.{item}\.{item}\.{item})".format(item=self.__ipv4_item)
-        # regex = r"""
-        #     ^({ipv6}:){6}{ipv4}$|
-        #     ^::({ipv6}:){0,4}{ipv4}$|
-        #     ^({ipv6}:):({ipv6}:){0,3}{ipv4}$|
-        #     ^({ipv6}:){2}:({ipv6}:){0,2}{ipv4}$|
-        #     ^({ipv6}:){3}:({ipv6}:){0,1}{ipv4}$|
-        #     ^({ipv6}:){4}:{ipv4}$|
-        #     ^({ipv6}:){7}{ipv6}$|^:((:{ipv6}){1,6}|:)$|
-        #     ^{ipv6}:((:{ipv6}){1,5}|:)$|
-        #     ^({ipv6}:){2}((:{ipv6}){1,4}|:)$|
-        #     ^({ipv6}:){3}((:{ipv6}){1,3}|:)$|
-        #     ^({ipv6}:){4}((:{ipv6}){1,2}|:)$|
-        #     ^({ipv6}:){5}:({ipv6})?$|
-        #     ^({ipv6}:){6}:$
-        # """.format(ipv4=ipv4_regex, ipv6=self.__ipv6_item)
-
-        if self.ip.startswith("::") or ure.search(self.__ipv6_item + ":", self.__ip):
-            return True
-        else:
-            return False
-
-    @option_lock(_socket_lock)
-    def __connect(self):
-        """Socket connect when method is TCP
-
-        Returns:
-            bool: True - success, False - falied
-        """
-        if self.__socket_args:
-            try:
-                self.__socket = usocket.socket(*self.__socket_args)
-                if self.__method == 'TCP':
-                    self.__socket.connect(self.__addr)
-                return True
-            except Exception as e:
-                usys.print_exception(e)
-
-        return False
-
-    @option_lock(_socket_lock)
-    def __disconnect(self):
-        """Socket disconnect
-
-        Returns:
-            bool: True - success, False - falied
-        """
-        if self.__socket is not None:
-            try:
-                self.__socket.close()
-                self.__socket = None
-                return True
-            except Exception as e:
-                usys.print_exception(e)
-                return False
-        else:
-            return True
-
-    @option_lock(_socket_lock)
-    def __send(self, data):
-        """Send data by socket.
-
-        Args:
-            data(bytes): byte stream
-
-        Returns:
-            bool: True - success, False - falied.
-        """
-        if self.__socket is not None:
-            try:
-                if self.__method == "TCP":
-                    write_data_num = self.__socket.write(data)
-                    if write_data_num == len(data):
-                        return True
-                elif self.__method == "UDP":
-                    send_data_num = self.__socket.sendto(data, self.__addr)
-                    if send_data_num == len(data):
-                        return True
-            except Exception as e:
-                usys.print_exception(e)
-
-        return False
-
-    def __read(self, bufsize=1024):
-        """Read data by socket.
-
-        Args:
-            bufsize(int): read data size.
-
-        Returns:
-            bytes: read data info
-        """
-        logger.debug("start read")
-        data = b""
-        if self.__socket is not None:
-            try:
-                while True:
-                    if data:
-                        self.__socket.settimeout(0.5)
-                    else:
-                        self.__socket.settimeout(self.__timeout)
-                    read_data = self.__socket.recv(bufsize)
-                    logger.debug("read_data: %s" % read_data)
-                    if read_data:
-                        data += read_data
-                    else:
-                        break
-            except Exception as e:
-                if e.args[0] != 110:
-                    usys.print_exception(e)
-                    logger.error("%s read falied. error: %s" % (self.__method, str(e)))
-
-        return data
-
-    def _downlink_thread_start(self):
-        """This function starts a thread to read the data sent by the server"""
-        pass
-
-    def _downlink_thread_stop(self):
-        """This function stop the thread that read the data sent by the server"""
-        pass
-
-    def _heart_beat_timer_start(self):
-        """This function starts a timer to send heart beat to server"""
-        pass
-
-    def _heart_beat_timer_stop(self):
-        """This function stop the timer that send heart beat to server"""
-        pass
-
-    def status(self):
-        """Get socket connection status
-
-        Returns:
-            [int]:
-                -1: Error
-                 0: Connected
-                 1: Connecting
-                 2: Disconnect
-        """
-        _status = -1
-        if self.__socket is not None:
-            try:
-                if self.__method == "TCP":
-                    socket_sta = self.__socket.getsocketsta()
-                    if socket_sta in range(4):
-                        # Connecting
-                        _status = 1
-                    elif socket_sta == 4:
-                        # Connected
-                        _status = 0
-                    elif socket_sta in range(5, 11):
-                        # Disconnect
-                        _status = 2
-                elif self.__method == "UDP":
-                    _status = 0
-            except Exception as e:
-                usys.print_exception(e)
-
-        return _status
-
-    def connect(self):
-        """Connect server and start downlink thread for server
-
-        Returns:
-            bool: True - success, False - failed
-        """
-        if self.__connect():
-            self._downlink_thread_start()
-            return True
-
-        return False
-
-    def disconnect(self):
-        """Disconnect server, than stop downlink thread and heart beat timer
-
-        Returns:
-            bool: True - success, False - failed
-        """
-        if self.__disconnect():
-            self._downlink_thread_stop()
-            self._heart_beat_timer_stop()
-            return True
-
-        return False
 
 
 class JTT808Base(SocketBase):
@@ -353,7 +74,6 @@ class JTT808Base(SocketBase):
         self.__read_thread = None
         self.__heart_beat_timer = osTimer()
         self.__heart_beat_is_running = False
-        self.__iter_serial_no = iter(range(99999))
         self.__callback = None
         self.__encryption = False
         self.__rsa_e = None
@@ -536,12 +256,12 @@ class JTT808Base(SocketBase):
             if self.__response_res.get(message_id) is not None:
                 if self.__response_res[message_id].get(serial_no) is not None:
                     data = self.__response_res[message_id].pop(serial_no)
-                    if not self.__response_res.get(message_id):
+                    if self.__response_res.get(message_id) is not None:
                         self.__response_res.pop(message_id)
                     break
                 elif self.__response_res[message_id].get(message_id) is not None:
                     data = self.__response_res[message_id].pop(message_id)
-                    if not self.__response_res.get(message_id):
+                    if self.__response_res.get(message_id) is not None:
                         self.__response_res.pop(message_id)
                     break
             utime.sleep_ms(100)
